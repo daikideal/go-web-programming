43e54f36bc12a0580d9d52b5d5b27e915e5ea462
daikideal <54879050+daikideal@users.noreply.github.com>
2021-10-23 17:41:55 +0900

    Add: テンプレートHTMLでのレスポンスを追加
    chapter-2 wip ~2.5

--- chitchat/main.go
+++ chitchat/main.go
@@ -1,31 +1,36 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
 	"html/template"
+	"net/http"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
-	_, err := session(w, r)
+	threads, err := data.Threads()
+	if err == nil {
+		_, err := session(w, r)
 
-	public_tmpl_files  := []string{"templates/layout.html",
-																"templates/public.navbar.html",
-																"templates/index.html",}
-	private_tmpl_files := []string{"templates/layout.html",
-																	"templates/private.navbar.html",
-																	"templates/index.html",}
-
-	var templates *template.Template
-	if err != nil {
-		templates = template.Must(template.ParseFiles(public_tmpl_files...))
-	} else {
-		templates = template.Must(template.ParseFiles(private_tmpl_files...))
+		if err != nil {
+			generateHTML(w, threads, "layout", "public.navbar", "index")
+		} else {
+			generateHTML(w, threads, "layout", "private.navbar", "index")
+		}
 	}
-	templates.ExecuteTemplate(w, "layout", threads)
+}
+
+func generateHTML(w http.ResponseWriter, data interface{}, fn ...string) {
+	var files []string
+	for _, file := range fn {
+		files = append(files, fmt.Sprintf("templates/%s.html", file))
+	}
+
+	templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(w, "layout", data)
 }
 
 func main() {
-	mux 	:= http.NewServeMux()
+	mux := http.NewServeMux()
 	files := http.FileServer(http.Dir(config.Static))
 	mux.Handle("/static/", http.StripPrefix("/static/", files))
 
@@ -44,7 +49,7 @@ func main() {
 	mux.HandleFunc("/thread/read", readThread)
 
 	server := &http.Server{
-		Addr: 	 "0.0.0.0:8080",
+		Addr:    "0.0.0.0:8080",
 		Handler: mux,
 	}
 	server.ListenAndServe()
