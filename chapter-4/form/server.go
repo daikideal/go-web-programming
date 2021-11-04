ebbca11f831c30b4703860fc1c16cb670a009d73
daikideal <54879050+daikideal@users.noreply.github.com>
2021-11-04 21:54:05 +0900

    Add: MultipartFormによるフォームデータのパースを追加
    chapter-4 wip ~4.3.3

--- chapter-4/form/server.go
+++ chapter-4/form/server.go
@@ -7,9 +7,18 @@ import (
 
 func process(w http.ResponseWriter, r *http.Request) {
 	// リクエストを解析してからFormフィールドにアクセスする必要がある
-	r.ParseForm()
-	fmt.Fprintln(w, r.Form) // => URLの値を取り扱う
-	fmt.Fprintln(w, r.PostForm) // => フォームのkey:valueのみを取り扱う, application/x-www-form-urlencodedしかサポートしていない
+	// r.ParseForm()
+	// fmt.Fprintln(w, r.Form) // => URLの値を取り扱う
+	// fmt.Fprintln(w, r.PostForm) // => フォームのkey:valueのみを取り扱う, application/x-www-form-urlencodedしかサポートしていない
+
+	// ParseForm()を呼ぶ必要がない
+	// fmt.Fprintln(w, r.PostFormValue("hello"))
+	// fmt.Fprintln(w, r.PostForm)
+
+	fmt.Fprintln(w, "(1)", r.FormValue("hello"))
+	fmt.Fprintln(w, "(2)", r.PostFormValue("hello"))
+	fmt.Fprintln(w, "(3)", r.PostForm)
+	fmt.Fprintln(w, "(4)", r.MultipartForm)
 }
 
 func main() {
