3640af33ad99ddff9dac080b975d9d0d0b267dc6
daikideal <54879050+daikideal@users.noreply.github.com>
2021-12-03 08:05:37 +0900

    Add: テストダブルによるHTTPテストを追加
    chapter-8 wip ~8.4.1

--- chapter-8/http_test/server.go
+++ chapter-8/http_test/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"net/http"
 	"path"
@@ -8,70 +9,89 @@ import (
 )
 
 type Post struct {
-	Id      int    `json:"id"`
-	Content string `json:"content"`
-	Author  string `json:"author"`
+	Db      *sql.DB // Postの全てのメソッドは構造体のフィールドの1つ`sql.DB`を使うようになる
+	Id      int     `json:"id"`
+	Content string  `json:"content"`
+	Author  string  `json:"author"`
+}
+
+type Text interface {
+	fetch(id int) (err error)
+	create() (err error)
+	update() (err error)
+	delete() (err error)
 }
 
 func main() {
-	server := http.Server{
-		Addr: "127.0.0.1:8080",
+	var err error
+	db, err := sql.Open(
+		"postgres",
+		"user=gwp dbname=gwp password=password sslmode=disable",
+	)
+	if err != nil {
+		panic(err)
 	}
-	http.HandleFunc("/post/", handleRequest)
+
+	server := http.Server{
+		Addr: ":8080",
+	}
+	// 1. 構造体Postを渡してhandleRequestを登録(間接的にsql.Dbへのポインタを渡す)
+	// => handleRequestへ"依存性の源を注入"
+	http.HandleFunc("/post/", handleRequest(&Post{Db: db}))
 	server.ListenAndServe()
 }
 
-// 1. リクエストを正しい関数に振り分けるためのハンドラ関数
-func handleRequest(w http.ResponseWriter, r *http.Request) {
-	var err error
-
-	// 正しい関数に分岐
-	switch r.Method {
-	case "GET":
-		err = handleGet(w, r)
-	case "POST":
-		err = handlePost(w, r)
-	case "PUT":
-		err = handlePut(w, r)
-	case "DELETE":
-		err = handleDelete(w, r)
-	}
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 1. インタフェースTextを渡す
+func handleRequest(t Text) http.HandlerFunc {
+	// 2. 正しいシグネチャの関数を返す
+	return func(w http.ResponseWriter, r *http.Request) {
+		var err error
+		switch r.Method {
+		case "GET":
+			// 3. 実際のハンドラにインタフェースTextを渡す
+			err = handleGet(w, r, t)
+		// case "POST":
+		// 	err = handlePost(w, r, t)
+		// case "PUT":
+		// 	err = handlePut(w, r, t)
+		// case "DELETE":
+		// 	err = handleDelete(w, r, t)
+		}
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 	}
 }
 
-// 2. 投稿の取り出し
-//
-// 検証:
-// 	curl -i -X GET http://127.0.0.1:8080/post/1
-func handleGet(w http.ResponseWriter, r *http.Request) (err error) {
+func (post *Post) fetch(id int) (err error) {
+	err = post.Db.QueryRow(
+		"select id, content, author from posts where id = $1",
+		id,
+	).Scan(&post.Id, &post.Content, &post.Author)
+	return
+}
+
+// 1. インタフェースTextを受け入れ
+func handleGet(w http.ResponseWriter, r *http.Request, post Text) (err error) {
 	id, err := strconv.Atoi(path.Base(r.URL.Path))
 	if err != nil {
 		return
 	}
-	// 1. データベースから構造体Postにデータ取得
-	post, err := retrieve(id)
+	// 2. 構造体Postからデータを取得
+	err = post.fetch(id)
 	if err != nil {
 		return
 	}
-	// 2. 構造体PostをJSON文字列に組み換え
 	output, err := json.MarshalIndent(&post, "", "\t\t")
 	if err != nil {
 		return
 	}
-	// 3. JSONをResponseWriterに書き出し
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(output)
 	return
 }
 
-// 3. 投稿の作成
-//
-// 検証:
-// 	curl -i -X POST -H "Content-Type: application/json" -d '{"content":"My first post", "author":"Sau Sheong"}' http://127.0.0.1:8080/post/
-// 	psql -U gwp -d gwp -c 'select * from posts;'
 func handlePost(w http.ResponseWriter, r *http.Request) (err error) {
 	len := r.ContentLength
 	// 1. バイト列を作成
@@ -91,27 +111,19 @@ func handlePost(w http.ResponseWriter, r *http.Request) (err error) {
 	return
 }
 
-// 4. 投稿の更新
-//
-// 検証:
-// 	curl -i -X PUT -H "Content-Type: application/json" -d '{"content":"Updated post", "author":"Sau Sheong"}' http://127.0.0.1:8080/post/1
 func handlePut(w http.ResponseWriter, r *http.Request) (err error) {
 	id, err := strconv.Atoi(path.Base(r.URL.Path))
 	if err != nil {
 		return
 	}
-	// 1. データベースから構造体Postにデータ取得
 	post, err := retrieve(id)
 	if err != nil {
 		return
 	}
 	len := r.ContentLength
 	body := make([]byte, len)
-	// 2. リクエスト本体からJSONデータを読み出し
 	r.Body.Read(body)
-	// 3. JSONデータを構造体Postに組み換え
 	json.Unmarshal(body, &post)
-	// 4. データベースを更新
 	err = post.update()
 	if err != nil {
 		return
@@ -120,21 +132,15 @@ func handlePut(w http.ResponseWriter, r *http.Request) (err error) {
 	return
 }
 
-// 5. 投稿の削除
-//
-// 検証:
-// 	curl -i -X DELETE http://127.0.0.1:8080/post/1
 func handleDelete(w http.ResponseWriter, r *http.Request) (err error) {
 	id, err := strconv.Atoi(path.Base(r.URL.Path))
 	if err != nil {
 		return
 	}
-	// 1. データベースから構造体Postにデータを取得
 	post, err := retrieve(id)
 	if err != nil {
 		return
 	}
-	// 2. データベースから投稿データを削除
 	err = post.delete()
 	if err != nil {
 		return
--- chapter-8/http_test/server_test.go
+++ chapter-8/http_test/server_test.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"os"
-	"strings"
+	// "strings"
 	"testing"
 )
 
@@ -20,39 +20,40 @@ func TestMain(m *testing.M) {
 
 // 事前処理
 func setUp() {
-	// 1. テストを実行するマルチプレクサを生成
-	mux = http.NewServeMux()
-	// 2. テスト対象のハンドラを付加
-	mux.HandleFunc("/post/", handleRequest)
-	// 3. 返されたHTTPレスポンスを取得
-	writer = httptest.NewRecorder()
+	// // 1. テストを実行するマルチプレクサを生成
+	// mux = http.NewServeMux()
+	// // 2. テスト対象のハンドラを付加
+	// mux.HandleFunc("/post/", handleRequest)
+	// // 3. 返されたHTTPレスポンスを取得
+	// writer = httptest.NewRecorder()
 }
 
 func TestHandleGet(t *testing.T) {
-	// 4. テストしたいハンドラ宛のリクエストを作成
+	mux := http.NewServeMux()
+	// 1. Postの代わりにFakePostを渡す
+	mux.HandleFunc("/post/", handleRequest(&FakePost{}))
+
+	writer := httptest.NewRecorder()
 	request, _ := http.NewRequest("GET", "/post/1", nil)
-	// 5. テスト対象のハンドラにリクエストを送信
 	mux.ServeHTTP(writer, request)
 
-	// 6. ResponseRecoderにより結果をチェック
 	if writer.Code != 200 {
 		t.Errorf("Response code is %v", writer.Code)
 	}
-	// レスポンスが期待値通りかをチェック
 	var post Post
 	json.Unmarshal(writer.Body.Bytes(), &post)
 	if post.Id != 1 {
-		t.Error("Cannnot retrieve JSON post")
+		t.Errorf("Cannot retrieve JSON post")
 	}
 }
 
-func TestHandlePut(t *testing.T) {
-	// JSONのコンテンツを送らなければならない
-	json := strings.NewReader(`{"content":"Updated post", "author":"Sau Sheong"}`)
-	request, _ := http.NewRequest("PUT", "/post/1", json)
-	mux.ServeHTTP(writer, request)
+// func TestHandlePut(t *testing.T) {
+// 	// JSONのコンテンツを送らなければならない
+// 	json := strings.NewReader(`{"content":"Updated post", "author":"Sau Sheong"}`)
+// 	request, _ := http.NewRequest("PUT", "/post/1", json)
+// 	mux.ServeHTTP(writer, request)
 
-	if writer.Code != 200 {
-		t.Errorf("Response code is %v", writer.Code)
-	}
-}
+// 	if writer.Code != 200 {
+// 		t.Errorf("Response code is %v", writer.Code)
+// 	}
+// }
